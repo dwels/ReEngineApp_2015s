e33fc52beed4dd9451c011ac4400daa0f10fefe2
Daniel Elsberry (RIT Student) <dwe5866@ad.rit.edu>
2016-03-10 22:52:08 -0500

    FInished Homework 6

--- A06_LERP/AppClass.cpp
+++ A06_LERP/AppClass.cpp
@@ -1,7 +1,7 @@
 #include "AppClass.h"
 void AppClass::InitWindow(String a_sWindowName)
 {
-	super::InitWindow("Assignment  06 - LERP"); // Window Name
+	super::InitWindow("Elsberry Assignment  06 - LERP"); // Window Name
 }
 
 void AppClass::InitVariables(void)
@@ -14,6 +14,35 @@ void AppClass::InitVariables(void)
 	m_pMeshMngr->LoadModel("Sorted\\WallEye.bto", "WallEye");
 
 	fDuration = 1.0f;
+
+	charMatrix = IDENTITY_M4;
+
+	std::vector<vector3> points = {		//size 11
+		vector3(-4.0f, -2.0f, 5.0f),	//0 time is 5
+		vector3(1.0f, -2.0f, 5.0f),		//1 time is 10
+		vector3(-3.0f, -1.0f, 3.0f),	//2 time is 15
+		vector3(2.0f, -1.0f, 3.0f),		//3 time is 20
+		vector3(-2.0f, 0.0f, 0.0f),		//4 ..25
+		vector3(3.0f, 0.0f, 0.0f),		//5	..30
+		vector3(-1.0f, 1.0f, -3.0f),	//6	..35
+		vector3(4.0f, 1.0f, -3.0f),		//7 ..40
+		vector3(0.0f, 2.0f, -5.0f),		//8 ..45
+		vector3(5.0f, 2.0f, -5.0f),		//9 ..50
+		vector3(1.0f, 3.0f, -5.0f)		//10 ..55
+	};
+
+	Points = points;
+
+	pntNum = 11;
+
+	sphere = new PrimitiveClass[pntNum];
+	sphereMat = new matrix4[pntNum];
+
+	for (int i = 0; i < pntNum; i++)
+	{
+		sphere[i].GenerateSphere(0.1f, 5, RERED);
+		sphereMat[i] = glm::translate(points[i]);
+	}
 }
 
 void AppClass::Update(void)
@@ -24,7 +53,7 @@ void AppClass::Update(void)
 
 	//Update the mesh manager's time without updating for collision detection
 	m_pMeshMngr->Update();
-#pragma region
+#pragma endregion
 
 #pragma region Does not need changes but feel free to change anything here
 	//Lets us know how much time has passed since the last call
@@ -33,10 +62,45 @@ void AppClass::Update(void)
 	//cumulative time
 	static double fRunTime = 0.0f; //How much time has passed since the program started
 	fRunTime += fTimeSpan; 
+	if (fRunTime > 55.0f) fRunTime = 0.0f;
 #pragma endregion
 
 #pragma region Your Code goes here
-	m_pMeshMngr->SetModelMatrix(IDENTITY_M4, "WallEye");
+	m_pMeshMngr->SetModelMatrix(charMatrix, "WallEye");
+
+	if (fRunTime <= 5) 
+		charMatrix = glm::translate(IDENTITY_M4, glm::lerp(Points[0], Points[1], static_cast<float>(fRunTime / 5)));
+
+	if (fRunTime <= 10 && fRunTime > 5)
+		charMatrix = glm::translate(IDENTITY_M4, glm::lerp(Points[1], Points[2], static_cast<float>((fRunTime -5) / 5)));
+
+	if (fRunTime <= 15 && fRunTime > 10)
+		charMatrix = glm::translate(IDENTITY_M4, glm::lerp(Points[2], Points[3], static_cast<float>((fRunTime - 10) / 5)));
+
+	if (fRunTime <= 20 && fRunTime > 15)
+		charMatrix = glm::translate(IDENTITY_M4, glm::lerp(Points[3], Points[4], static_cast<float>((fRunTime - 15) / 5)));
+	
+	if (fRunTime <= 25 && fRunTime > 20)
+		charMatrix = glm::translate(IDENTITY_M4, glm::lerp(Points[4], Points[5], static_cast<float>((fRunTime - 20) / 5)));
+
+	if (fRunTime <= 30 && fRunTime > 25)
+		charMatrix = glm::translate(IDENTITY_M4, glm::lerp(Points[5], Points[6], static_cast<float>((fRunTime - 25) / 5)));
+
+	if (fRunTime <= 35 && fRunTime > 30)
+		charMatrix = glm::translate(IDENTITY_M4, glm::lerp(Points[6], Points[7], static_cast<float>((fRunTime - 30) / 5)));
+
+	if (fRunTime <= 40 && fRunTime > 35)
+		charMatrix = glm::translate(IDENTITY_M4, glm::lerp(Points[7], Points[8], static_cast<float>((fRunTime - 35) / 5)));
+
+	if (fRunTime <= 45 && fRunTime > 40)
+		charMatrix = glm::translate(IDENTITY_M4, glm::lerp(Points[8], Points[9], static_cast<float>((fRunTime - 40) / 5)));
+
+	if (fRunTime <= 50 && fRunTime > 45)
+		charMatrix = glm::translate(IDENTITY_M4, glm::lerp(Points[9], Points[10], static_cast<float>((fRunTime - 45) / 5)));
+
+	if (fRunTime <= 55 && fRunTime > 50)
+		charMatrix = glm::translate(IDENTITY_M4, glm::lerp(Points[10], Points[0], static_cast<float>((fRunTime - 50) / 5)));
+
 #pragma endregion
 
 #pragma region Does not need changes but feel free to change anything here
@@ -75,6 +139,14 @@ void AppClass::Display(void)
 		break;
 	}
 	
+	matrix4 mProjection = m_pCameraMngr->GetProjectionMatrix();
+	matrix4 mView = m_pCameraMngr->GetViewMatrix();
+
+	for (int i = 0; i < pntNum; i++)
+	{
+		sphere[i].Render(mProjection, mView, sphereMat[i]);
+	}
+
 	m_pMeshMngr->Render(); //renders the render list
 
 	m_pGLSystem->GLSwapBuffers(); //Swaps the OpenGL buffers
@@ -82,5 +154,15 @@ void AppClass::Display(void)
 
 void AppClass::Release(void)
 {
+	if (sphere != nullptr)
+	{
+		delete[] sphere; //new[] delete[], new()->delete
+		sphere = nullptr;
+	}
+	if (sphereMat != nullptr)
+	{
+		delete[] sphereMat; //new[] delete[], new()->delete
+		sphereMat = nullptr;
+	}
 	super::Release(); //release the memory of the inherited fields
 }
