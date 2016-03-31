f70f070c87fbcb6c28852a8c429f8bd0aedd7726
Daniel Elsberry (RIT Student) <dwe5866@ad.rit.edu>
2016-03-31 12:08:23 -0400

    finished E10

--- E10_Projections/AppClass.cpp
+++ E10_Projections/AppClass.cpp
@@ -15,7 +15,11 @@ void AppClass::InitVariables(void)
 
 	//Calculate the first projections
 	m_m4Projection = glm::perspective(45.0f, 1080.0f / 768.0f, 0.01f, 1000.0f);
-	m_m4View = glm::lookAt(glm::vec3(0.0f, 0.0f, 15.0f), glm::vec3(0.0f, 0.0f, 14.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	
+	m_m4View = glm::lookAt(
+		glm::vec3(0.0f, 0.0f, 15.0f), //move
+		glm::vec3(0.0f, 0.0f, 14.0f), //facing 
+		glm::vec3(0.0f, 1.0f, 0.0f)); //rotation
 }
 
 void AppClass::Update(void)
